6112e09d50e14809e73986a38fdc3916cc44e23e
Alison Major <alison.major@gmail.com>
2020-09-18 13:59:28 -0400

    Rename ElectricKettleTest for consistency
    Wk3

--- src/com/command/teas/ElectricKettleTest.java
+++ src/com/command/teas/ElectricKettleTest.java
@@ -4,7 +4,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-class TestElectricKettle {
+class ElectricKettleTest {
 	ElectricKettle kettle = new ElectricKettle();
 
 	@Test
