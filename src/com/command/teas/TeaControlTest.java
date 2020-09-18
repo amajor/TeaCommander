d0ffaa1c12449b74eafb9e8e00b2a6c035d2bc4a
Alison Major <alison.major@gmail.com>
2020-09-18 15:15:35 -0400

    Update friends button to always set temp for green tea
    We don't want to ruin our green tea! Let's set that button to adjust the temperature for the tea before we dispense our tea leaves.
    
    Wk3

--- src/com/command/teas/TeaControlTest.java
+++ src/com/command/teas/TeaControlTest.java
@@ -2,6 +2,7 @@ package com.command.teas;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class TeaControlTest extends TeaControl {
@@ -13,19 +14,20 @@ class TeaControlTest extends TeaControl {
 	
 	TeaLeafDispenser dispenser = new TeaLeafDispenser();
 	TeaLeafDispenseForOneCommand dispenseForOne = new TeaLeafDispenseForOneCommand(dispenser);
-	TeaLeafDispenseForFriendsCommand dispenseForFriends = new TeaLeafDispenseForFriendsCommand(dispenser);
+	TeaLeafDispenseForFriendsCommand dispenseForFriends = new TeaLeafDispenseForFriendsCommand(dispenser, kettle);
 
+	@BeforeEach
 	void initialize() {
-		control.setCommand(0,  kettleOnCommand, kettleOffCommand);
-		control.setCommand(1,  dispenseForOne, dispenseForFriends);
+		control.setCommand(0, kettleOnCommand, kettleOffCommand);
+		control.setCommand(1, dispenseForOne, dispenseForFriends);
 		
 		System.out.println(control);
 	}
 	
 	@Test
 	void testTeaControlForOne() {
-		initialize();
-		
+		System.out.println("=== Making Black Tea for One! ===");
+
 		// Make tea for me!
 		control.onButtonWasPushed(0);
 		assertEquals(kettle.on, true);
@@ -34,6 +36,23 @@ class TeaControlTest extends TeaControl {
 		control.onButtonWasPushed(1);
 		assertEquals(dispenser.type, "English Breakfast");
 		assertEquals(dispenser.amount, "1 teaspoon");
+		
+		control.offButtonWasPushed(0);
+		assertEquals(kettle.on, false);
+	}
+	
+	@Test
+	void testTeaControlForFriends() {
+		System.out.println("=== Making Green Tea for Friends! ===");
+
+		// Make tea for friends!
+		control.onButtonWasPushed(0);
+		assertEquals(kettle.on, true);
+		
+		control.offButtonWasPushed(1);
+		assertEquals(kettle.temperature, 165);
+		assertEquals(dispenser.type, "Green Tea");
+		assertEquals(dispenser.amount, "4 teaspoons");
 		
 		control.offButtonWasPushed(0);
 		assertEquals(kettle.on, false);
--- src/com/command/teas/TeaLeafDispenseForFriendsCommand.java
+++ src/com/command/teas/TeaLeafDispenseForFriendsCommand.java
@@ -2,14 +2,19 @@ package com.command.teas;
 
 public class TeaLeafDispenseForFriendsCommand implements Command {
 	TeaLeafDispenser dispenser;
+	ElectricKettle kettle;
 	
-	public TeaLeafDispenseForFriendsCommand(TeaLeafDispenser dispenser) {
+	public TeaLeafDispenseForFriendsCommand(TeaLeafDispenser dispenser, ElectricKettle kettle) {
 		this.dispenser = dispenser;
+		this.kettle = kettle;
 	}
 	
 	public void execute() {
 		System.out.println("Choosing green tea.");
 		dispenser.chooseGreenTea();
+
+		// Set temperature for green tea
+		kettle.setGreenTeaTemp();
 
 		System.out.println("Dispensing tea for 1 pot.");
 		dispenser.dispenseForOnePot();
